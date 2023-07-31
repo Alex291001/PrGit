66bd8ba2d824e97038ba19decbf390ecc6e2a121
Alex2910 <alexantonioangel2910@gmail.com>
2023-07-31 11:14:32 -0600

    Nueva version en main

--- menu.kt
+++ menu.kt
@@ -3,4 +3,6 @@ fun main() {
     println("nueva actualizacion de Git")
     println("Nueva modificacion despues del login")
     println("Segunda modificacion despues de crear el login")
+
+    println("nueva version 3 de main")
 }
