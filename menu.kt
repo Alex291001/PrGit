592106332e4345ad32b8aba76280a68cd6c8579b
Alex2910 <alexantonioangel2910@gmail.com>
2023-07-31 14:09:00 -0600

    Correccion solucionadado de merge

--- menu.kt
+++ menu.kt
@@ -4,9 +4,7 @@ fun main() {
     println("Nueva modificacion despues del login")
     println("Segunda modificacion despues de crear el login")
 
-<<<<<<< HEAD
-    println("Nueva version 3 de Logins")
-=======
-    println("nueva version 3 de main")
->>>>>>> main
+
+    println("nueva version 3 de main corregido")
+
 }
